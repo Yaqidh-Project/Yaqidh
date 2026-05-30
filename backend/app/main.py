e5f227337c8a268a4fa71aa7f1f22f77ccf38c28
Aliyah <AliyahAlabdali24@gmail.com>
2026-05-30 16:01:26 +0300

    Fix CORS policy error

--- backend/app/main.py
+++ backend/app/main.py
@@ -52,13 +52,12 @@ origins = [
     "https://yaqidh.vercel.app",  
     "http://localhost:5173",      
     "http://127.0.0.1:5173",
-    "*",
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    allow_credentials=True, 
     allow_methods=["*"],          
     allow_headers=["*"],
 )
